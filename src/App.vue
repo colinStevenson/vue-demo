f9cc4229c3949c09641092e33c11dc8e884a8781
Colin Stevenson <colin.stevenson@gmail.com>
2016-11-04 11:22:37 -0600

    Remove styling, add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container" id="app">
+    <h1 class="page-header">{{ msg }}</h1>
+    <div class="panel panel-primary">
+      <header class="panel-heading">
+        <h1 class="panel-title">Essential Links</h1>
+      </header>
+      <div class="panel-body">
+        <ul class="list-unstyled">
+          <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+          <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+          <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+          <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="panel panel-primary">
+      <header class="panel-heading">
+        <h1 class="panel-title">Ecosystem</h1>
+      </header>
+      <div class="panel-body">
+        <ul class="list-unstyled">
+          <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+          <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+          <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+          <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,37 +35,12 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Vue Demo'
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
